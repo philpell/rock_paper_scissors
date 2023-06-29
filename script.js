645736c22b9344a95ab50a04d1b775f22a8f8541
Phil Pell <134700971+philpell@users.noreply.github.com>
2023-06-29 15:00:31 +0100

    UI working with buttons

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 // create a game of rock, paper, scissors
 //Set variables
 let computerSelection;
-let playerSelection = prompt("Choose your weapon!");
+let playerSelection; // = alert("Choose your weapon!");
 let computerScore = 0;
 let playerScore = 0;
 const myArray = ["Rock", "Paper", "Scissors"];
 
-// Runs the multiple games function
-game()
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    playerSelection = button.id;
+    game();
+});
+});
+
+const roundResult = document.createElement('div');
+roundResult.style.textAlign = 'center';
+roundResult.style.font = 'bold';
+roundResult.style.fontSize = '30px';
+
+const currentScore = document.createElement('div');
+currentScore.style.textAlign = 'center';
+currentScore.style.font = 'bold';
+currentScore.style.fontSize = '20px';
+currentScore.style.color = 'red'
+
+const gameResult = document.createElement('div');
+gameResult.style.textAlign = 'center';
+gameResult.style.font = 'bold';
+gameResult.style.fontSize = '30px';
+
+container.appendChild(roundResult);
+container.appendChild(currentScore);
+container.appendChild(gameResult);
 
 // Create a random computer choice
 function computerChoice() {
@@ -16,13 +44,15 @@ function computerChoice() {
 
 // Create function to compare the selections for single game  
 function playRound(playerSelection, computerSelection) {
-
+if (playerScore == 5|| computerScore == 5){
+  alert('The game is over, please refresh the page');
+} else {
 //  To ensure the format of the inputted word is not case sensitive
   computerSelection = computerChoice().toLowerCase();
   playerSelection = playerSelection.toLowerCase();
 
   if (computerSelection == playerSelection) {
-    console.log("Tie game!");
+    roundResult.textContent = "Tie game!";
 
   } else if (
     (computerSelection == "rock" && playerSelection == "scissors") ||
@@ -30,8 +60,8 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection == "paper" && playerSelection == "rock")
   ) {
     computerScore = ++computerScore;
-    console.log(`Bad luck, ${computerSelection} beats ${playerSelection}.`);
-    console.log(`Player 1 score: ${playerScore}, Computer score: ${computerScore}`)
+    roundResult.textContent = `Bad luck, ${computerSelection} beats ${playerSelection}.`;
+    currentScore.textContent = `Player 1 score: ${playerScore}, Computer score: ${computerScore}`;
     return computerScore
 
     } else if(
@@ -40,27 +70,28 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection == "scissors" && playerSelection == "rock")
   ) {
     playerScore = ++playerScore;
-    console.log(`Well Done! Your ${playerSelection} beats ${computerSelection}.`);
-    console.log(`Player 1 score: ${playerScore}, Computer score: ${computerScore}`)
+    roundResult.textContent = `Well Done! Your ${playerSelection} beats ${computerSelection}.`;
+    currentScore.textContent = `Player 1 score: ${playerScore}, Computer score: ${computerScore}`;
     return playerScore
 
   } else {
-      console.log(`Please enter either rock, paper or scissors`);
+    roundResult.textContent = `Please enter either rock, paper or scissors`;
     }
 }
+} 
 
 // Create function to record scores, first to five
 function game() {
     while ((playerScore < 5) || (computerScore < 5)) {
         playRound(playerSelection, computerSelection)
             if(playerScore == 5){
-                console.log(`Player 1 wins by ${playerScore} to ${computerScore}.`)
+              gameResult.textContent = `Player 1 wins by ${playerScore} to ${computerScore}.`;
                 break
             } else if(computerScore == 5){
-                console.log(`Computer wins by ${computerScore} to ${playerScore}.`)
+              gameResult.textContent = `Computer wins by ${computerScore} to ${playerScore}.`;
                 break
             } else{
-                playerSelection = prompt("Choose your weapon!");
+                break;
 }
 }
 }
