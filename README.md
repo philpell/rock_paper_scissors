07535d10ab04bc91ee07515cda62b756f3eac58d
Phil Pell <134700971+philpell@users.noreply.github.com>
2023-07-08 20:27:30 +0100

    Change to design and additional pages

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+Attributions:
 
+Lobster Image by brgfx on Freepik <a href="https://www.freepik.com/free-vector/red-lobster-cartoon-sticker_21306007.htm#query=lobster%20cartoon&position=2&from_view=keyword&track=ais">Image by brgfx</a> on Freepik
+Game image by katemangostar on Freepik: <a href="https://www.freepik.com/free-vector/realistic-beautiful-sea-view-summer-vacation-concept_2438179.htm#query=beach%20cartoon&position=1&from_view=search&track=ais">Image by katemangostar</a> on Freepik
+Seaweed image from <a href="https://www.flaticon.com/free-icons/seaweed" title="seaweed icons">Seaweed icons created by Freepik - Flaticon</a>
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// create a game of rock, paper, scissors
+// // create a game of lobster, pebble, seaweed
 //Set variables
 let computerSelection;
-let playerSelection; // = alert("Choose your weapon!");
+let playerSelection;
 let computerScore = 0;
 let playerScore = 0;
-const myArray = ["Rock", "Paper", "Scissors"];
 
+const myArray = ["pebble", "seaweed", "lobster"];
 const buttons = document.querySelectorAll('button')
 
 buttons.forEach((button) => {
@@ -13,85 +13,124 @@ buttons.forEach((button) => {
   // and for each one we add a 'click' listener
   button.addEventListener('click', () => {
     playerSelection = button.id;
+    container.appendChild(start);
+    container.removeChild(start);
     game();
 });
 });
 
+const roundComment = document.createElement('div');
+roundComment.style.textAlign = 'center';
+roundComment.style.font = '700';
+roundComment.style.fontSize = '50px';
+roundComment.style.color = '#FF9800';
+roundComment.style.textShadow = '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black';
+
 const roundResult = document.createElement('div');
 roundResult.style.textAlign = 'center';
-roundResult.style.font = 'bold';
-roundResult.style.fontSize = '30px';
+roundResult.style.font = '700';
+roundResult.style.fontSize = '70px';
+roundResult.style.color = '#FF9800';
+roundResult.style.textShadow = '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black';
 
 const currentScore = document.createElement('div');
 currentScore.style.textAlign = 'center';
-currentScore.style.font = 'bold';
-currentScore.style.fontSize = '20px';
-currentScore.style.color = 'red'
+currentScore.style.font = '700';
+currentScore.style.fontSize = '70px';
+currentScore.style.color = '#FF9800';
+currentScore.style.textShadow = '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black';
 
 const gameResult = document.createElement('div');
 gameResult.style.textAlign = 'center';
-gameResult.style.font = 'bold';
-gameResult.style.fontSize = '30px';
+gameResult.style.font = '700';
+gameResult.style.fontSize = '100px';
+gameResult.style.color = '#FF9800';
+gameResult.style.textShadow = '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black';
 
+const start = document.createElement('div');
+start.style.textAlign = 'center';
+start.style.font = '700';
+start.style.fontSize = '50px';
+start.style.color = '#FF9800';
+start.style.textShadow = '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black';
+
+container.appendChild(roundComment);
 container.appendChild(roundResult);
 container.appendChild(currentScore);
 container.appendChild(gameResult);
+container.appendChild(start);
 
-// Create a random computer choice
+// // Create a random computer choice
 function computerChoice() {
   return myArray[~~(Math.random() * myArray.length)];
 }
 
-// Create function to compare the selections for single game  
+// // Create function to compare the selections for single game  
 function playRound(playerSelection, computerSelection) {
 if (playerScore == 5|| computerScore == 5){
-  alert('The game is over, please refresh the page');
+
 } else {
-//  To ensure the format of the inputted word is not case sensitive
+// //  To ensure the format of the inputted word is not case sensitive
   computerSelection = computerChoice().toLowerCase();
   playerSelection = playerSelection.toLowerCase();
 
   if (computerSelection == playerSelection) {
     roundResult.textContent = "Tie game!";
-
+    roundResult.style.fontSize = '50px';
+    roundComment.textContent = ``
+    
   } else if (
-    (computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "scissors" && playerSelection == "paper") ||
-    (computerSelection == "paper" && playerSelection == "rock")
+    (computerSelection == "pebble" && playerSelection == "lobster") ||
+    (computerSelection == "lobster" && playerSelection == "seaweed") ||
+    (computerSelection == "seaweed" && playerSelection == "pebble")
   ) {
     computerScore = ++computerScore;
-    roundResult.textContent = `Bad luck, ${computerSelection} beats ${playerSelection}.`;
-    currentScore.textContent = `Player 1 score: ${playerScore}, Computer score: ${computerScore}`;
-    return computerScore
+    roundResult.style.fontSize = '70px';
+    roundComment.textContent = `Bad luck!`
+    roundResult.textContent = `The computer's ${computerSelection} beats your ${playerSelection}.`;
+    currentScore.textContent = `Player 1 score: ${playerScore}  -  Computer score: ${computerScore}`;
+    return computerScore;
 
     } else if(
-    (computerSelection == "paper" && playerSelection == "scissors") ||
-    (computerSelection == "rock" && playerSelection == "paper") ||
-    (computerSelection == "scissors" && playerSelection == "rock")
+    (computerSelection == "seaweed" && playerSelection == "lobster") ||
+    (computerSelection == "pebble" && playerSelection == "seaweed") ||
+    (computerSelection == "lobster" && playerSelection == "pebble")
   ) {
     playerScore = ++playerScore;
-    roundResult.textContent = `Well Done! Your ${playerSelection} beats ${computerSelection}.`;
-    currentScore.textContent = `Player 1 score: ${playerScore}, Computer score: ${computerScore}`;
+    roundResult.style.fontSize = '70px';
+    roundComment.textContent = `Well Done!`
+    roundResult.textContent = `Your ${playerSelection} beats the computer's ${computerSelection}.`;
+    currentScore.textContent = `Player 1 score: ${playerScore} - Computer score: ${computerScore}`;
     return playerScore
-
+    
   } else {
-    roundResult.textContent = `Please enter either rock, paper or scissors`;
+    window.location.reload();
     }
 }
 } 
 
-// Create function to record scores, first to five
+// // Create function to record scores, first to five
 function game() {
     while ((playerScore < 5) || (computerScore < 5)) {
         playRound(playerSelection, computerSelection)
             if(playerScore == 5){
-              gameResult.textContent = `Player 1 wins by ${playerScore} to ${computerScore}.`;
+              roundResult.textContent = ``;
+              currentScore.textContent = ``;
+              gameResult.textContent = `Player 1 wins by ${playerScore} to ${computerScore}`;
                 break
             } else if(computerScore == 5){
-              gameResult.textContent = `Computer wins by ${computerScore} to ${playerScore}.`;
+              gameResult.textContent = `Computer wins by ${computerScore} to ${playerScore}`;
+              currentScore.textContent = ``;
+              roundResult.textContent = ``;
                 break
             } else{
                 break;
 }
 }
 }
+
+function instruction() {
+    //   Inform player of what to do
+  start.textContent = `Please select one of the items above to begin`;
+}
+
